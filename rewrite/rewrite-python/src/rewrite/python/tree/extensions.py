e7d0e8a47a9530210a555f76dfeaeb82ae026d10
Knut Wannheden <knut@moderne.io>
2024-08-15 17:28:57 +0200

    Resolve another cyclic dependency

--- rewrite/rewrite-python/src/rewrite/python/tree/extensions.py
+++ rewrite/rewrite-python/src/rewrite/python/tree/extensions.py
@@ -1,25 +1,29 @@
-from typing import Optional, TypeVar
+from typing import Optional, TypeVar, TYPE_CHECKING
 
 from rewrite.java.tree import J, JContainer, JRightPadded, JLeftPadded, Space
 from .support_types import PyContainer, PyRightPadded, PyLeftPadded, PySpace
-from ..visitor import PythonVisitor
 
+if TYPE_CHECKING:
+    from ..visitor import PythonVisitor
 
 T = TypeVar('T')
 J2 = TypeVar('J2', bound=J)
 
 
-def visit_container(v: PythonVisitor, container: Optional[JContainer[J2]], loc: PyContainer.Location, p) -> Optional[JContainer[J2]]:
+def visit_container(v: 'PythonVisitor', container: Optional[JContainer[J2]], loc: PyContainer.Location, p) -> Optional[
+    JContainer[J2]]:
     return None
 
 
-def visit_right_padded(v: PythonVisitor, right: Optional[JRightPadded[T]], loc: PyRightPadded.Location, p) -> Optional[JRightPadded[T]]:
+def visit_right_padded(v: 'PythonVisitor', right: Optional[JRightPadded[T]], loc: PyRightPadded.Location, p) -> Optional[
+    JRightPadded[T]]:
     return None
 
 
-def visit_left_padded(v: PythonVisitor, left: Optional[JLeftPadded[T]], loc: PyLeftPadded.Location, p) -> Optional[JLeftPadded[T]]:
+def visit_left_padded(v: 'PythonVisitor', left: Optional[JLeftPadded[T]], loc: PyLeftPadded.Location, p) -> Optional[
+    JLeftPadded[T]]:
     return None
 
 
-def visit_space(v: PythonVisitor, space: Optional[Space], loc: PySpace.Location, p):
+def visit_space(v: 'PythonVisitor', space: Optional[Space], loc: PySpace.Location, p):
     return None
