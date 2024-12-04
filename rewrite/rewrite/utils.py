c2842aed28f53f1b63ece437a194f7af37573622
Knut Wannheden <knut@moderne.io>
2024-12-04 09:38:12 +0100

    Allow `list_map` to be called with a mapping function with two parameters

--- rewrite/rewrite/utils.py
+++ rewrite/rewrite/utils.py
@@ -1,3 +1,4 @@
+import inspect
 from typing import Callable, TypeVar, List, Union
 from uuid import UUID, uuid4
 
@@ -7,15 +8,28 @@ def random_id() -> UUID:
 
 T = TypeVar('T')
 
-def list_map(fn: Callable[[T], Union[T, None]], lst: List[T]) -> List[T]:
+# Define a type that allows both single and two-argument callables
+FnType = Union[Callable[[T], Union[T, None]], Callable[[T, int], Union[T, None]]]
+
+def list_map(fn: FnType, lst: List[T]) -> List[T]:
     changed = False
     mapped_lst = []
-    for original in lst:
-        new = fn(original)
-        if new is not None:
-            mapped_lst.append(new)
-            changed |= new is not original
-        else:
-            changed = True
+
+    if len(inspect.signature(fn).parameters) == 1:
+        for original in lst:
+            new = fn(original)
+            if new is not None:
+                mapped_lst.append(new)
+                changed |= new is not original
+            else:
+                changed = True
+    else:
+        for index, original in enumerate(lst):
+            new = fn(original, index)
+            if new is not None:
+                mapped_lst.append(new)
+                changed |= new is not original
+            else:
+                changed = True
 
     return mapped_lst if changed else lst
