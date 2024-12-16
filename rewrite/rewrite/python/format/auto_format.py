193193efa9caeee05fe5712509cc9a22e51fe06b
Niels de Bruin <nielsdebruin@gmail.com>
2024-12-16 12:21:37 +0100

    Add RemoveTrailingWhitespaceVisitor (#100)
    * Add RemoveTrailingWhitespaceVisitor
    
    * Add pre/post visit methods
    
    * Fix attribute
    
    * Call RemoveTrailingWhiteSpaceVisitor in AutoFormatVisitor
    
    * Update rewrite/rewrite/python/format/remove_trailing_whitespace_visitor.py
    
    Co-authored-by: Knut Wannheden <knut@moderne.io>
    
    * Fix for trailing comma and clean
    
    ---------
    
    Co-authored-by: Knut Wannheden <knut@moderne.io>

--- rewrite/rewrite/python/format/auto_format.py
+++ rewrite/rewrite/python/format/auto_format.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 from .blank_lines import BlankLinesVisitor
 from .normalize_format import NormalizeFormatVisitor
+from .remove_trailing_whitespace_visitor import RemoveTrailingWhitespaceVisitor
 from .spaces_visitor import SpacesVisitor
 from .normalize_tabs_or_spaces import NormalizeTabsOrSpacesVisitor
 from .. import TabsAndIndentsStyle
@@ -32,4 +33,5 @@ class AutoFormatVisitor(PythonVisitor):
             cu.get_style(TabsAndIndentsStyle) or IntelliJ.tabs_and_indents(),
             self._stop_after
         ).visit(tree, p, self._cursor.fork())
+        tree = RemoveTrailingWhitespaceVisitor(self._stop_after).visit(tree, self._cursor.fork())
         return tree
