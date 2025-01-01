1ea18387eb9f51231c989a4d7147751d4a922b7d
leanne <leanne@moderne.io>
2025-01-01 09:24:05 -0800

    Adding the dependencycheck plugin
    The vulnerability-analysis github action is failing to run because this repository doesn't have the dependencyCheckAggregate task. This task is provided by the org.owasp.dependencycheck gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("org.openrewrite.build.root") version("latest.release")
     id("org.openrewrite.build.java-base") version("latest.release")
+    id("org.owasp.dependencycheck") version "latest.release"
 }
 
 allprojects {
