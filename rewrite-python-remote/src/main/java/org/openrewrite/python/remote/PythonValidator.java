46bb6b051f718463bca64b5e1f48e100cf9139ff
Tim te Beek <tim@moderne.io>
2025-08-07 12:52:31 +0200

    Expect `J.VariableDeclarations` for now

--- rewrite-python-remote/src/main/java/org/openrewrite/python/remote/PythonValidator.java
+++ rewrite-python-remote/src/main/java/org/openrewrite/python/remote/PythonValidator.java
@@ -277,7 +277,7 @@ class PythonValidator<P> extends PythonIsoVisitor<P> {
 
     @Override
     public Py.MatchCase.Pattern visitMatchCasePattern(Py.MatchCase.Pattern pattern, P p) {
-        visitAndValidate(pattern.getChildren(), Expression.class, p);
+        visitAndValidate(pattern.getChildren(), J.VariableDeclarations.class, p);
         return pattern;
     }
 
